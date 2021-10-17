b869c63f4108bdb84d90323e7ef1a8b5153a8079
Eugune <biletskijzena@protonmail.com>
2021-10-17 21:19:09 +0300

    Виконане завдання

---
+++ ISMPrakt02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISMPrakt02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите a:");
+            double a  = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите c:");
+            double c = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите d:");
+            double d = Convert.ToDouble(Console.ReadLine());
+
+            double x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - (a * a) / (b * b);
+            double y = (5 * (a + b) * (c - b)) / (1 / 2 * c) + d * d * (a * a - b * b) / (b - a);
+            double z = (((x * x - 2 * x) - 4 * (x * x * x * x + 1)) * (1 - b)) / (5 * a + 3 * b);
+            double r = (1/2*a+3/4*b-7/5)/(3*c+1)+ 1 / (a - c);
+            Console.Write($"x = {x}, y = {y}, z ={z}, r = {r}  ");
+        }
+    }
+}
---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace QuadraticEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, D, x1, x2;
+            Console.WriteLine("Введите a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите c: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            D = Math.Pow(b, 2) - 4 * a * c;
+            if (D > 0)
+            {
+                x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                Console.WriteLine($"x1 = {x1}; x2 = {x2}");
+            }
+            else
+            {
+                if (D == 0)
+                {
+                    x1 = -b / (2 * a);
+                    Console.WriteLine($"x ={x1}");
+                }
+                Console.WriteLine("Уравнение не имеет значений");
+            }
+        }
+    }
+}
