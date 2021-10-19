c1566ba280b5d3b9459306ed69fdba58446ca82f
Eugune Beletskiy <91693377+defild9@users.noreply.github.com>
2021-10-19 21:10:59 +0300

    Update Program.cs

--- ISMPrakt02/Program.cs
+++ ISMPrakt02/Program.cs
@@ -22,7 +22,7 @@ namespace ISMPrakt02
             double x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - Math.Pow(a,2)/ Math.Pow(b,2);
             double y = (5 * (a + b) * (c - b)) / (1 / 2 * c) + Math.Pow(d,2)* (Math.Pow(a,2) - Math.Pow(b,2)) / (b - a);
             double z = ((Math.Pow((Math.Pow(x,2) - 2 * x),3) - 4 * (Math.Pow(x,4)+ 1)) * (1 - b)) / (5 * a + 3 * b);
-            double r = (1.00/2*a+3.00/4*b-7/5)/(3*c+1)+ 1 / (a - c);
+            double r = (1.00/2*a+3.00/4*b-7.0/5)/(3*c+1)+ 1 / (a - c);
             Console.Write($"x = {x}, y = {y}, z ={z}, r = {r}  ");
         }
     }
