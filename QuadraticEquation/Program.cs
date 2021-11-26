29d238b14e9fa1ac419e209ad810485b19a2970e
Eugune Beletskiy <91693377+defild9@users.noreply.github.com>
2021-11-26 20:59:11 +0200

    Виконане завдання

--- QuadraticEquation/Program.cs
+++ QuadraticEquation/Program.cs
@@ -20,18 +20,18 @@ namespace QuadraticEquation
             c = Convert.ToDouble(Console.ReadLine());
 
             D = Math.Pow(b, 2) - 4 * a * c;
-            if (D > 0)
+            if (D == 0)
             {
-                x1 = (-b + Math.Sqrt(D)) / (2 * a);
-                x2 = (-b - Math.Sqrt(D)) / (2 * a);
-                Console.WriteLine($"x1 = {x1}; x2 = {x2}");
+                 x1 = -b / (2 * a);
+                 Console.WriteLine($"x ={x1}");
             }
             else
             {
-                if (D == 0)
+                if (D > 0)
                 {
-                    x1 = -b / (2 * a);
-                    Console.WriteLine($"x ={x1}");
+                    x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                    Console.WriteLine($"x1 = {x1}; x2 = {x2}");
                 }
                 Console.WriteLine("Уравнение не имеет значений");
             }
